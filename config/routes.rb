7910debe28548a604a93c6717f1f80950a6e91e0
Ubuntu <tomonori1001@gmail.com>
2020-07-17 05:38:28 +0000

    tabinavi03

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'home_pages#home'
   get '/signup',  to:'users#new'
-  get '/login',   to:'home_pages#login'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   get '/search',  to:'home_pages#search'
   resources :users
 end
